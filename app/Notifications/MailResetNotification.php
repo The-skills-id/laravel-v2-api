f749619816cd31b198d2ea33363085c34508b3ba
Denta Maulana <54609779+dntffm@users.noreply.github.com>
2021-07-02 14:07:34 +0700

    Update MailResetNotification.php

--- app/Notifications/MailResetNotification.php
+++ app/Notifications/MailResetNotification.php
@@ -41,7 +41,7 @@ class MailResetNotification extends ResetPassword
      */
     public function toMail($notifiable)
     {
-        $link = url( "https://the-skills.id/auth/change-password?token=".$this->token );
+        $link = url( "https://the-skills.id/auth/reset-password/change-password?token=".$this->token );
         return ( new MailMessage )
             ->subject( 'Notifikasi Reset Password' )
             ->line( "Halo! Kamu menerima email ini karena akun theskills anda meminta untuk pergantian password." )
