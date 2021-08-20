ba72e0d754579450746383725451a6a1f745db69
dntffm <dentamauuulana99@gmail.com>
2021-08-20 21:25:14 +0700

    softdeletes course and its child

--- app/Minicourse.php
+++ app/Minicourse.php
@@ -3,8 +3,9 @@
 namespace app;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Minicourse extends Model{
+    use SoftDeletes;
     public function subcourse()
     {
         return $this->belongsTo('App\Subcourse');
